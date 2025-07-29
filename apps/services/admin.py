95ef8c825ff9a4efa05b7df8ec695f7f458c145d
Davi-D18 <luizdavi.code@gmail.com>
2025-07-29 17:01:53 -0300

    feat(admin): adiciona configuração do modelo Service no Django Admin

--- apps/services/admin.py
+++ apps/services/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from apps.services.models.services import TypeService
+from apps.services.models.services import TypeService, Service
 
 
 @admin.register(TypeService)
 class TypeServiceAdmin(admin.ModelAdmin):
     list_display = ("nome", "criado_em")
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "estudante", "tipo_servico", "criado_em", "descricao")
+    list_filter = ("tipo_servico", "estudante")
