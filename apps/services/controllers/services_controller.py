531e353ae368baae39a4d2c49587002b42d90bbf
Davi-D18 <luizdavi.code@gmail.com>
2025-08-02 20:16:03 -0300

    refactor(services): ajusta controle de acesso aos serviços
    - Remove retrieve das permissões IsOwner
    - Adiciona validação customizada para serviços inativos
    - Restringe acesso de público externo a serviços inativos
    - Simplifica get_queryset e rota /meus/

--- apps/services/controllers/services_controller.py
+++ apps/services/controllers/services_controller.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 from rest_framework.decorators import action
+from rest_framework.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -15,7 +16,7 @@ class ServiceViewSet(ModelViewSet):
 
     def get_permissions(self):
         # nas ações retrieve/update/partial_update/destroy, aplica também IsOwner
-        if self.action in ['retrieve', 'update', 'partial_update', 'destroy']:
+        if self.action in ['update', 'partial_update', 'destroy']:
             return [permissions.IsAuthenticated(), IsOwner()]
         return super().get_permissions()
 
@@ -28,25 +29,32 @@ class ServiceViewSet(ModelViewSet):
         )
 
         # rota padrão de listagem: só os ativos, de qualquer usuário
-        if self.action == 'list':
+        if self.action =='list':
             return qs.filter(ativo=True)
 
-        # rota /servicos/meus/: todos (ativos ou não) do usuário logado
-        if self.action == 'buscar_servicos_usuario':
-            usuario = get_object_or_404(CustomerUser, usuario=self.request.user)
-            return qs.filter(estudante=usuario)
+        return qs
 
-        # retrieve, update, partial_update e destroy: todos (ativos ou não) do próprio usuário
-        if self.action in ['retrieve', 'update', 'partial_update', 'destroy']:
-            usuario = get_object_or_404(CustomerUser, usuario=self.request.user)
-            return qs.filter(estudante=usuario)
+    
+    def retrieve(self, request, *args, **kwargs):
+        service = self.get_object()
 
-        # por segurança, não expor nada inesperado
-        return qs.none()
+        if not service.ativo:
+            user_cust = get_object_or_404(CustomerUser, usuario=request.user)
+            if user_cust.tipo_usuario == 'publico_externo':
+                raise PermissionDenied({
+                    'permission': 'Você não tem permissão para ver esse serviço'
+                })
+
+        return super().retrieve(request, *args, **kwargs)
 
     @action(detail=False, methods=['get'], url_path='meus')
     def buscar_servicos_usuario(self, request):
-        qs = self.get_queryset()
+        qs = Service.objects.select_related(
+            'estudante__usuario',
+            'estudante__universidade',
+            'estudante__curso',
+            'tipo_servico'
+        ).filter(estudante=get_object_or_404(CustomerUser, usuario=request.user))
         serializer = self.get_serializer(qs, many=True)
         return Response(serializer.data)
 
